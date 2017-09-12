b30dacb4f7c85a70513284a8ce62c92d25fb396e
caikeke <ckcaikeke@163.com>
2017-09-12 16:38:35 +0800

    修改JS交互

--- src/android/WebViewActivity.java
+++ src/android/WebViewActivity.java
@@ -1,13 +1,19 @@
 package com.webview.yhck;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -42,6 +48,7 @@ public class WebViewActivity extends Activity {
   private String urlHost;
   //显示页面URL
   private String mURL;
+  private String paramAction;
   //JS方法和参数
   private String mFuncAddParams;
   WebView mWebview;
@@ -81,6 +88,7 @@ public class WebViewActivity extends Activity {
 
   private void initView() {
     String paramJSON = getIntent().getStringExtra("Param");
+     paramAction = getIntent().getStringExtra("Action");
     try {
       JSONObject jsonObject = new JSONObject(paramJSON);
       mNumber = Integer.valueOf(jsonObject.get("badgeNum").toString());
@@ -88,7 +96,9 @@ public class WebViewActivity extends Activity {
       mTitle = jsonObject.get("title").toString();
       mURL = jsonObject.get("URL").toString();
       //2017.09.12:添加参数：android 调用JS方法
-
+      if("webViewAddParam".equals(paramAction)){
+        mFuncAddParams = jsonObject.get("funcAddParam").toString();
+      }
     } catch (JSONException e) {
       e.printStackTrace();
     }
@@ -104,12 +114,15 @@ public class WebViewActivity extends Activity {
     mWebSettings.setJavaScriptEnabled(true);
     //不使用缓存:
     mWebview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+    // 设置允许JS弹窗
+    mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
     mWebview.loadUrl(mURL);
     // android 5.0以上默认不支持Mixed Content
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
       mWebview.getSettings().setMixedContentMode(
         WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
     }
+
     //复写shouldOverrideUrlLoading()方法，使得打开网页时不调用系统浏览器， 而是在本WebView中显示
     mWebview.setWebViewClient(new WebViewClient() {
       @Override
@@ -140,6 +153,62 @@ public class WebViewActivity extends Activity {
       @Override
       public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
         handler.proceed();// 接受所有网站的证书
+      }
+
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        //2017.09.12:添加参数：android 调用JS方法
+        if("webViewAddParam".equals(paramAction)){
+          sendInfoToJs();
+        }
+      }
+    });
+
+    //添加客户端支持
+    mWebview.setWebChromeClient(new WebChromeClient(){
+      @Override
+      public boolean onJsAlert(WebView view, String url, String message,final JsResult result) {
+        AlertDialog.Builder mAlert = new AlertDialog.Builder(WebViewActivity.this);
+        mAlert.setTitle("Alert");
+        mAlert.setMessage(message);
+        mAlert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            result.confirm();
+          }
+        });
+        mAlert.setCancelable(false);
+        mAlert.create().show();
+        return true;
+      }
+    });
+    mWebview.addJavascriptInterface(new JsInterface(this), "Android");
+  }
+  private class JsInterface {
+    private Context mContext;
+
+    public JsInterface(Context context) {
+      this.mContext = context;
+    }
+
+    //在js中调用window.Android.showInfoFromJs(name)，便会触发此方法。
+    @JavascriptInterface
+    public void showInfoFromJs(String name) {
+      Toast.makeText(mContext, name, Toast.LENGTH_SHORT).show();
+    }
+  }
+  //在java中调用js代码
+  public void sendInfoToJs() {
+    final String jsonData = "在java中调用js代码";
+    //必须另开线程进行JS方法调用(否则无法调用)
+    mWebview.post(new Runnable() {
+      @Override
+      public void run() {
+        // 注意调用的JS方法名要对应上
+        // 调用javascript的callJS()方法
+        // mWebview.loadUrl("javascript:callJS('"+jsonData+"')");
+        mWebview.loadUrl("javascript:"+mFuncAddParams);
       }
     });
   }
